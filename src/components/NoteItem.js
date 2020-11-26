4b3b9532ac989432f1aeb93d7ec0c3f1b2a61a69
Benyamin Lea <lea.benyamin@student-cs.fr>
2020-11-26 14:48:56 +0200

    Milestone 4 done

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,24 +1,62 @@
 import "./NoteItem.css";
 import React from "react";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 class NoteItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
   onDelete(event) {
     this.props.onDeleteNote(event.target.id);
+  }
+  onOpenModal() {
+    this.setState({ isOpen: true });
+  }
+
+  onCloseModal() {
+    this.setState({ isOpen: false });
   }
 
   render() {
     return (
-      <li>
-        <button
-          id={this.props.dateCreated}
-          onClick={(event) => this.onDelete(event)}
+      <div>
+        <li onClick={() => this.onOpenModal()}>
+          <button
+            id={this.props.dateCreated}
+            onClick={(event) => this.onDelete(event)}
+          >
+            Delete
+          </button>
+          <div className="title">{this.props.title}</div>
+          <div className="date">{this.props.dateReadable}</div>
+          <div className="note">{this.props.note}</div>
+        </li>
+        <Modal
+          isOpen={this.state.isOpen}
+          onRequestClose={() => this.onCloseModal()}
+          style={{
+            overlay: { backgroundColor: "grey" },
+            content: {
+              width: "100px",
+              height: "100px",
+              top: "25%",
+              left: "25%",
+              right: "auto",
+              bottom: "auto",
+            },
+          }}
         >
-          X
-        </button>
-        <div className="title">{this.props.title}</div>
-        <div className="date">{this.props.dateReadable}</div>
-        <div className="note">{this.props.note}</div>
-      </li>
+          <button onClick={() => this.onCloseModal()}>Close</button>
+          <div className="title">{this.props.title}</div>
+          <div className="date">{this.props.dateReadable}</div>
+          <div className="note">{this.props.note}</div>
+        </Modal>
+      </div>
     );
   }
 }
